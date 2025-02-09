0d092d17fbabe268bd9df6f55e60a2510ba2ad29
Kama <huavuss@gmail.com>
2025-02-09 23:48:35 +0700

    Query doanh thu

--- src/main/java/com/example/demo/repository/HoaDonRepository.java
+++ src/main/java/com/example/demo/repository/HoaDonRepository.java
@@ -8,18 +8,46 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface HoaDonRepository extends JpaRepository<HoaDonEntity, UUID>, JpaSpecificationExecutor<HoaDonEntity> {
-//
-@Query("SELECT FUNCTION('DATE', h.ngayThanhToan) AS ngay, SUM(h.tongTien) " +
-        "FROM HoaDonEntity h WHERE h.trangThai = 1 " +
-        "GROUP BY FUNCTION('DATE', h.ngayThanhToan) " +
-        "ORDER BY FUNCTION('DATE', h.ngayThanhToan)")
-List<Object[]> doanhThuTheoNgay();
+
+    @Query("SELECT SUM(h.tongTien) FROM HoaDonEntity h " +
+            "WHERE h.trangThai = 1 AND FUNCTION('DATE', h.ngayThanhToan) = FUNCTION('CURRENT_DATE')")
+    BigDecimal doanhThuNgayHienTai();
+
+
+    @Query("SELECT SUM(h.tongTien) FROM HoaDonEntity h " +
+            "WHERE h.trangThai = 1 AND YEAR(h.ngayThanhToan) = YEAR(CURRENT_DATE) " +
+            "AND MONTH(h.ngayThanhToan) = MONTH(CURRENT_DATE)")
+    BigDecimal doanhThuThangHienTai();
+
+
+    @Query("SELECT SUM(h.tongTien) FROM HoaDonEntity h " +
+            "WHERE h.trangThai = 1 AND YEAR(h.ngayThanhToan) = YEAR(CURRENT_DATE)")
+    BigDecimal doanhThuNamHienTai();
+
+
+    @Query("SELECT SUM(h.tongTien) FROM HoaDonEntity h " +
+            "WHERE h.trangThai = 1 AND FUNCTION('DATE', h.ngayThanhToan) = :ngay")
+    BigDecimal doanhThuTheoNgayCuThe(LocalDate ngay);
+
+
+    @Query("SELECT SUM(h.tongTien) FROM HoaDonEntity h " +
+            "WHERE h.trangThai = 1 AND FUNCTION('DATE', h.ngayThanhToan) " +
+            "BETWEEN :startDate AND :endDate")
+    BigDecimal doanhThuTheoKhoangNgay(LocalDate startDate, LocalDate endDate);
+
+
+    @Query("SELECT FUNCTION('DATE', h.ngayThanhToan), SUM(h.tongTien)" +
+            " FROM HoaDonEntity h WHERE h.trangThai = 1 GROUP BY FUNCTION" +
+            "('DATE', h.ngayThanhToan) ORDER BY FUNCTION('DATE', h.ngayThanhToan)")
+    List<Object[]> doanhThuTheoNgay();
+
 
 
 
