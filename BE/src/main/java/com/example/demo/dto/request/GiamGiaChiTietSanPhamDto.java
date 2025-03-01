f51c8b226917173be11775f8c8ad634f8be8d2bd
thanhnvph25467 <thanhnvph25467@fpt.edu.vn>
2025-03-01 23:16:07 +0700

    fix

--- BE/src/main/java/com/example/demo/dto/request/GiamGiaChiTietSanPhamDto.java
+++ BE/src/main/java/com/example/demo/dto/request/GiamGiaChiTietSanPhamDto.java
@@ -20,6 +20,6 @@ public class GiamGiaChiTietSanPhamDto extends PageDto{
     private UUID id;
     private BigDecimal soTienDaGiam;
     private Integer trangThai;
-    private GiayDto giayDto;
+    private GiayChiTietDto giayDto;
     private GiamGiaSanPhamDto giamGiaSanPhamDto;
 }
