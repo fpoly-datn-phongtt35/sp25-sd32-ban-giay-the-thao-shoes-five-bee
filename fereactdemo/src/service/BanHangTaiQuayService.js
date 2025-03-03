00b98a5f83c2ecb58dd03fe3caa32ab1c41f6c52
phuongtt <phuongttph21351@fpt.edu.vn>
2025-03-03 11:44:18 +0700

    Fix case-sensitive file issue

--- fereactdemo/src/service/BanHangTaiQuayService.js
+++ fereactdemo/src/service/BanHangTaiQuayService.js
@@ -2,26 +2,47 @@ import axios from "../axiosConfig";
 
 const REST_API_BASE_URL = "/api/ban-hang-tai-quay";
 
-export const createHoaDonBanHangTaiQuay = () => 
-  axios.post(`${REST_API_BASE_URL}/create`);
+export const thanhToanTaiQuay = (idHoaDon, hoaDonRequest) => {
+  return axios.post(
+    `${REST_API_BASE_URL}/thanh-toan/${idHoaDon}`,
+    hoaDonRequest
+  );
+};
 
-export const themSanPhamVaoHoaDon = (idHoaDon, idSanPham) =>
-  axios.post(`${REST_API_BASE_URL}/add-product/${idHoaDon}?idSanPham=${idSanPham}`);
+export const createHoaDonBanHangTaiQuay = () => {
+  return axios.post(`${REST_API_BASE_URL}/create`);
+};
 
-export const updateSoLuongGiay = (idHoaDonChiTiet, isIncrease) =>
-  axios.put(`${REST_API_BASE_URL}/update-quantity/${idHoaDonChiTiet}?isIncrease=${isIncrease}`);
+export const themSanPhamVaoHoaDon = (idHoaDon, idSanPham) => {
+  return axios.post(
+    `${REST_API_BASE_URL}/add-product/${idHoaDon}`,
+    null, // Không gửi body
+    { params: { idSanPham } } // Gửi idSanPham trong query params
+  );
+};
 
-export const getListHoaDonCho = () =>
-  axios.get(`${REST_API_BASE_URL}/list`);
+export const updateSoLuongGiay = (idHoaDonChiTiet, isIncrease) => {
+  return axios.put(
+    `${REST_API_BASE_URL}/update-quantity/${idHoaDonChiTiet}`,
+    null,
+    {
+      params: { isIncrease },
+    }
+  );
+};
 
-export const deleteHoaDonCho = (idHoaDon) =>
-  axios.delete(`${REST_API_BASE_URL}/delete/${idHoaDon}`);
+export const getListHoaDonCho = () => {
+  return axios.get(`${REST_API_BASE_URL}/list`);
+};
 
-export const deleteAllHoaDonCho = (idHoaDons) =>
-  axios.delete(`${REST_API_BASE_URL}/delete-all`, { data: idHoaDons });
+export const deleteHoaDonCho = (idHoaDon) => {
+  return axios.delete(`${REST_API_BASE_URL}/delete/${idHoaDon}`);
+};
 
-export const deleteHoaDonChiTiet = (idHoaDonChiTiet) =>
-  axios.delete(`${REST_API_BASE_URL}/delete-detail/${idHoaDonChiTiet}`);
+export const deleteAllHoaDonCho = (idHoaDons) => {
+  return axios.delete(`${REST_API_BASE_URL}/delete-all`, { data: idHoaDons });
+};
 
-export const thanhToanTaiQuay = (idHoaDon, hoaDonRequest) =>
-  axios.post(`${REST_API_BASE_URL}/thanh-toan/${idHoaDon}`, hoaDonRequest); 
+export const deleteHoaDonChiTiet = (idHoaDonChiTiet) => {
+  return axios.delete(`${REST_API_BASE_URL}/delete-detail/${idHoaDonChiTiet}`);
+};
