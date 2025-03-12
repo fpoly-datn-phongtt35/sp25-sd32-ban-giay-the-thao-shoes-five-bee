7b04c815040b9c4c4f077ce38ab96fea1b80838d
HuaVu <vuhhph43699@fpt.edu.vn>
2025-03-13 02:14:53 +0700

    loc update

--- BE/src/main/java/com/example/demo/controller/GiayChiTietController.java
+++ BE/src/main/java/com/example/demo/controller/GiayChiTietController.java
@@ -3,26 +3,23 @@ package com.example.demo.controller;
 import com.example.demo.dto.request.GiayChiTietDto;
 import com.example.demo.entity.GiayChiTietEntity;
 import com.example.demo.service.GiayChiTietService;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/giay-chi-tiet")
 @RequiredArgsConstructor
 public class GiayChiTietController {
   private final GiayChiTietService giayChiTietService;
-
-  @PutMapping("/update-bien-the")
-  public ResponseEntity<?> updateBienThe(
-      @PathVariable UUID id, @RequestParam Integer soLuong, @RequestParam BigDecimal giaBan) {
-    return ResponseEntity.ok(giayChiTietService.updateSoLuongVaGaiaBan(id, soLuong, giaBan));
-  }
 
   @PostMapping("/getAll")
   public ResponseEntity<?> getAll() {
@@ -32,7 +29,8 @@ public class GiayChiTietController {
   @PostMapping("/search")
   public ResponseEntity<?> findAndPageChatLieu(@RequestBody GiayChiTietDto giayChiTietDto) {
     Pageable pageable = PageRequest.of(giayChiTietDto.getPage(), giayChiTietDto.getSize());
-    return ResponseEntity.ok(giayChiTietService.findByPagingCriteria(giayChiTietDto, pageable));
+    return ResponseEntity.ok(
+            giayChiTietService.findByPagingCriteria(giayChiTietDto, pageable));
   }
 
   @PostMapping("/add")
@@ -58,14 +56,22 @@ public class GiayChiTietController {
   }
 
   @PostMapping("/{id}/anhGiayChiTiet")
-  public ResponseEntity<?> anhGiay(
-      @PathVariable("id") UUID id, @RequestBody List<UUID> anhGiayIds) {
-    return ResponseEntity.ok(giayChiTietService.assignAnhGiay(id, anhGiayIds));
+  public ResponseEntity<?> anhGiay(@PathVariable("id") UUID id, @RequestBody List<UUID> anhGiayIds) {
+    return ResponseEntity.ok(giayChiTietService.assignAnhGiay(id,anhGiayIds));
   }
-
+  
   @GetMapping("/getAll/{giayId}")
   public ResponseEntity<List<GiayChiTietEntity>> getGiayChiTietByGiayId(@PathVariable UUID giayId) {
     List<GiayChiTietEntity> chiTietList = giayChiTietService.getAllGiayChiTietByGiayId(giayId);
     return ResponseEntity.ok(chiTietList);
+  }
+  @GetMapping("/loc")
+  public ResponseEntity<List<GiayChiTietEntity>> filterGiayChiTiet(
+          @RequestParam(required = false) UUID mauSacId,
+          @RequestParam(required = false) UUID kichCoId,
+          @RequestParam(required = false) UUID thuongHieuId) {
+
+    List<GiayChiTietEntity> result = giayChiTietService.filterGiayChiTiet(mauSacId, kichCoId, thuongHieuId);
+    return ResponseEntity.ok(result);
   }
 }
--- BE/src/main/java/com/example/demo/service/GiayChiTietService.java
+++ BE/src/main/java/com/example/demo/service/GiayChiTietService.java
@@ -2,30 +2,30 @@ package com.example.demo.service;
 
 import com.example.demo.dto.request.GiayChiTietDto;
 import com.example.demo.dto.response.PageResponse;
+import com.example.demo.entity.AnhGiayEntity;
 import com.example.demo.entity.GiayChiTietEntity;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
+import com.example.demo.entity.GiayEntity;
 import lombok.NonNull;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface GiayChiTietService {
-  GiayChiTietEntity updateSoLuongVaGaiaBan(UUID id, Integer soLuong, BigDecimal giaBan);
+    List<GiayChiTietEntity> getAll();
 
-  List<GiayChiTietEntity> getAll();
+    GiayChiTietEntity add(GiayChiTietDto giayChiTietDto);
 
-  GiayChiTietEntity add(GiayChiTietDto giayChiTietDto);
+    GiayChiTietEntity update(GiayChiTietDto giayChiTietDto);
 
-  GiayChiTietEntity update(GiayChiTietDto giayChiTietDto);
+    GiayChiTietEntity detail(GiayChiTietDto giayChiTietDto);
 
-  GiayChiTietEntity detail(GiayChiTietDto giayChiTietDto);
+    GiayChiTietEntity delete(GiayChiTietDto giayChiTietDto);
 
-  GiayChiTietEntity delete(GiayChiTietDto giayChiTietDto);
+    PageResponse<GiayChiTietEntity> findByPagingCriteria(
+            GiayChiTietDto giayChiTietDto, Pageable pageable);
+    GiayChiTietEntity assignAnhGiay(@NonNull UUID id, @NonNull List<UUID> anhGiayIds);
+    List<GiayChiTietEntity> getAllGiayChiTietByGiayId(UUID giayId);
 
-  PageResponse<GiayChiTietEntity> findByPagingCriteria(
-      GiayChiTietDto giayChiTietDto, Pageable pageable);
-
-  GiayChiTietEntity assignAnhGiay(@NonNull UUID id, @NonNull List<UUID> anhGiayIds);
-
-  List<GiayChiTietEntity> getAllGiayChiTietByGiayId(UUID giayId);
+    List<GiayChiTietEntity> filterGiayChiTiet(UUID mauSacId, UUID kichCoId, UUID thuongHieuId);
 }
