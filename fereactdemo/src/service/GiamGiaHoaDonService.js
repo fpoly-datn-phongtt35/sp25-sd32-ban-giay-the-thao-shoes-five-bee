255678f08fe27175a7689bc0e57c80fec2b2bc09
phuongtt <phuongttph21351@fpt.edu.vn>
2025-03-17 12:05:02 +0700

    Add the best discount code

--- fereactdemo/src/service/GiamGiaHoaDonService.js
+++ fereactdemo/src/service/GiamGiaHoaDonService.js
@@ -6,13 +6,13 @@ export const getGiamGiaHoaDon = () => axios.post(`${REST_API_BASE_URL}/getAll`);
 
 export const getGiamGia = async (ma = "") => {
   try {
-    const response = await axios.get(`${REST_API_BASE_URL}`, {
+    const response = await axios.get(`${REST_API_BASE_URL}/giam-gia`, {
       params: { ma },
     });
-    return response.data;
+    return response;
   } catch (error) {
     console.error("Lỗi khi gọi API giảm giá:", error);
-    return null;
+    throw error;
   }
 };
 
