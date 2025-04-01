3c867aa0d1f391b31ab8de4a3f84d251409baafb
NguyenVanThanh <nvanthanh15102003@gmail.com>
2025-04-02 01:16:01 +0700

    fix giam gia san pham

--- BE/src/main/java/com/example/demo/repository/GiamGiaChiTietSanPhamRepository.java
+++ BE/src/main/java/com/example/demo/repository/GiamGiaChiTietSanPhamRepository.java
@@ -3,23 +3,34 @@ package com.example.demo.repository;
 import com.example.demo.entity.GiamGiaChiTietSanPhamEntity;
 import com.example.demo.entity.GiayEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface GiamGiaChiTietSanPhamRepository
     extends JpaRepository<GiamGiaChiTietSanPhamEntity, UUID>,
         JpaSpecificationExecutor<GiamGiaChiTietSanPhamEntity> {
 
+  @Query(
+      "SELECT g FROM GiamGiaChiTietSanPhamEntity g WHERE g.giayChiTiet.id = :id AND g.trangThai = 0 ORDER BY g.ngayBatDau DESC")
+  List<GiamGiaChiTietSanPhamEntity> findByGiayChiTiet(@Param("id") UUID id);
 
-  @Query("SELECT g FROM GiamGiaChiTietSanPhamEntity g WHERE g.giayChiTiet.id = :id")
-  GiamGiaChiTietSanPhamEntity findByGiayChiTiet(@Param("id") UUID id);
+  @Query(
+          "SELECT g FROM GiamGiaChiTietSanPhamEntity g WHERE g.chuongTrinhGiamSanPhamEntity.id = :id AND g.trangThai = 0")
+  List<GiamGiaChiTietSanPhamEntity> findByGiayChiTietByGiamGiaSanPham(@Param("id") UUID id);
+
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM GiamGiaChiTietSanPhamEntity f WHERE f.chuongTrinhGiamSanPhamEntity.id = :id")
+  void deleteByGiamGiaSanPhamId(@Param("id") UUID id);
 
   Optional<GiamGiaChiTietSanPhamEntity> findByGiayChiTiet_Id(UUID giayChiTietId);
-
 }
